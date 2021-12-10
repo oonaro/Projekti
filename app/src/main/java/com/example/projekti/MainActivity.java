9c798c8f26e0f58d4927d6fad8d92a17f97b8288
oonarosenblad <Oona.Rosenblad@metropolia.fi>
2021-12-10 13:44:13 +0200

    Image buttonit vaihdettu ja liikunta ja fiilis activitya muokattu

--- app/src/main/java/com/example/projekti/MainActivity.java
+++ app/src/main/java/com/example/projekti/MainActivity.java
@@ -2,13 +2,91 @@ package com.example.projekti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+/**
+ * Tähän projektin nimi
+ * @author oonarosenblad
+ * joku kommentti tässä
+ */
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton buttonLiikunta;
+    private ImageButton buttonUni;
+    private ImageButton buttonRavinto;
+    private ImageButton buttonFiilis;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonLiikunta = (ImageButton) findViewById(R.id.liikunta);
+        buttonUni = (ImageButton) findViewById(R.id.Uni);
+        buttonRavinto = (ImageButton) findViewById(R.id.Ravinto);
+        buttonFiilis = (ImageButton) findViewById(R.id.Fiilis);
+
+
+        /**
+         * Ensimmäisen activityn avaaminen
+         */
+
+        buttonLiikunta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Liikunta.class);
+                startActivity(intent);
+
+            }
+        });
+
+        /**
+         * Toisen activityn avaaminen imagebuttonia painamalla
+         */
+
+        buttonUni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Uni.class);
+                startActivity(intent);
+
+            }
+        });
+
+        /**
+         * Kolmannen activityn avaaminen imagebuttonia painamalla
+         */
+
+
+        buttonRavinto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Ravinto.class);
+                startActivity(intent);
+
+            }
+        });
+
+        /**
+         * Neljännen activityn avaaminen imagebuttonia painamalla
+         */
+        buttonFiilis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Fiilis.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
