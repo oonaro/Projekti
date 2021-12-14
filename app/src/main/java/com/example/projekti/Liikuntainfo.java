0c8fb3a6a8bd33b3626dbe5f30e3b91f84e2ea54
aleksijohannes <aleksi.alanko@hotmail.com>
2021-12-14 09:08:32 +0200

    Ravinto-Activity päivitetty

--- app/src/main/java/com/example/projekti/Liikuntainfo.java
+++ app/src/main/java/com/example/projekti/Liikuntainfo.java
@@ -16,20 +16,26 @@ public class Liikuntainfo extends AppCompatActivity {
         TextView t1;
         TextView t2;
         TextView t3;
+        TextView ruokaTV;
+        TextView kaloritTV;
 
-        t1 = findViewById(R.id.textView12);
-        t2 = findViewById(R.id.textView9);
-        t3 = findViewById(R.id.textView13);
+        t1 = findViewById(R.id.liikuntaMinuutitTV);
+        t2 = findViewById(R.id.fiilisTV);
+        t3 = findViewById(R.id.unenMaaraTV);
+        ruokaTV = findViewById(R.id.ruokaTV);
+        kaloritTV = findViewById(R.id.kaloritTV);
 
         SharedPreferences sp = getApplicationContext().getSharedPreferences("Nimi tähä", Context.MODE_PRIVATE);
         String liikuntaminuutit = sp.getString("liikuntaminuutit", "");
         String fiilis = sp.getString("fiilis", "");
         String unenmaara = sp.getString("unenmäärä", "");
+        String ruoka = sp.getString("ruoka", "");
+        String kalorit = sp.getString("kalorit", "");
 
-
-
-        t1.setText(liikuntaminuutit);
+        t1.setText(liikuntaminuutit + " min");
         t2.setText(fiilis);
-        t3.setText(unenmaara);
+        t3.setText(unenmaara + " tuntia");
+        ruokaTV.setText(ruoka);
+        kaloritTV.setText(kalorit + " kaloria");
     }
 }
--- app/src/main/java/com/example/projekti/Ravinto.java
+++ app/src/main/java/com/example/projekti/Ravinto.java
@@ -2,13 +2,48 @@ package com.example.projekti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Ravinto extends AppCompatActivity {
+
+    EditText ruoka;
+    EditText kalorit;
+    Button tallenna;
+    SharedPreferences sp;
+    String ruokaStr;
+    String kaloritStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ravinto);
+
+        ruoka = findViewById(R.id.editText_ruoka);
+        kalorit = findViewById(R.id.editText_kalorit);
+        tallenna = findViewById(R.id.button_tallenna);
+
+        sp = getSharedPreferences("Nimi tähä", Context.MODE_PRIVATE);
+
+        tallenna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ruokaStr = ruoka.getText().toString();
+                kaloritStr = kalorit.getText().toString();
+
+                SharedPreferences.Editor editor = sp.edit();
+
+                editor.putString("ruoka", ruokaStr);
+                editor.putString("kalorit", kaloritStr);
+                editor.commit();
+                Toast.makeText(Ravinto.this, "Information saved", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
     }
 }
